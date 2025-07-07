2ff2a4a7317f9598880e01bef16935508d0bf2f9
nozcat <noz@noz.ai>
2025-07-07 16:43:10 -0600

    Use API key for user

--- index.js
+++ index.js
@@ -5,23 +5,18 @@ const { LinearClient } = linearSdk
 dotenv.config()
 
 const LINEAR_API_KEY = process.env.LINEAR_API_KEY
-const LINEAR_EMAIL = process.env.LINEAR_EMAIL
 
 async function main () {
   const linearClient = new LinearClient({
     apiKey: LINEAR_API_KEY
   })
 
-  const users = await linearClient.users({ filter: { email: { eq: LINEAR_EMAIL } } })
-  const user = users.nodes[0]
-  if (!user) {
-    console.error(`User with email ${LINEAR_EMAIL} not found`)
-    return
-  }
+  const user = await linearClient.viewer
 
   const issues = await linearClient.issues({ filter: { assignee: { id: { eq: user.id } } } })
+
   for (const issue of issues.nodes) {
-    console.log(issue.identifier, issue.title)
+    console.log(issue.identifier, issue.title, issue.branchName)
   }
 }
 
