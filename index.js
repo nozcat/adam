b53ddef9a00575e1ba474d1e676fcc82af8915bf
nozcat <noz@noz.ai>
2025-07-07 14:17:04 -0600

    Query issues for user

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const linearSdk = require('@linear/sdk')
 const dotenv = require('dotenv')
 const { LinearClient } = linearSdk
 
-async function main () {
-  dotenv.config()
+dotenv.config()
 
+const LINEAR_API_KEY = process.env.LINEAR_API_KEY
+const LINEAR_EMAIL = process.env.LINEAR_EMAIL
+
+async function main () {
   const linearClient = new LinearClient({
-    apiKey: process.env.LINEAR_API_KEY
+    apiKey: LINEAR_API_KEY
   })
 
-  const user = await linearClient.viewer
-  console.log(user)
+  const users = await linearClient.users({ filter: { email: { eq: LINEAR_EMAIL } } })
+  const user = users.nodes[0]
+  if (!user) {
+    console.error(`User with email ${LINEAR_EMAIL} not found`)
+    return
+  }
+
+  const issues = await linearClient.issues({ filter: { assignee: { id: { eq: user.id } } } })
+  for (const issue of issues.nodes) {
+    console.log(issue.identifier, issue.title)
+  }
 }
 
 main()
